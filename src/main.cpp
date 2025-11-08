b4a99ed64b6ceb5081c3795aa09b01ffb23a28c7
Harveker <franco.endrigo.r@gmail.com>
2025-11-08 02:47:17 -0300

    Refactor main.cpp for improved readability and structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#define TDIR 10  //trás direita
-#define FDIR 11  //frente direita
-#define TESQ 9   //trás esquerda
-#define FESQ 6   //frente esquerda
+#define TDIR 10 // trás direita
+#define FDIR 11 // frente direita
+#define TESQ 9  // trás esquerda
+#define FESQ 6  // frente esquerda
 #define LD A2
 #define CD A3
 #define CE A4
@@ -13,110 +13,197 @@
 #include <math.h>
 #include "AcksenButton.h"
 
-//ENUMERAÇÕES E STRUCTS
+// ENUMERAÇÕES E STRUCTS-------------------------------------------------------------------------------------------------------------------------------
 
-//Struct para controle das I/Os do robô
-struct controlIO {
-  enum Bobinas {TDIR_,FDIR_,TESQ_,FESQ_};   // enumerando para facilitar a identificação das bobinas
-  enum Sensores {LD_,CD_,CE_,LE_};  // enumerando para facilitar a identificação dos sensores
-  int bobinas[4]  = {TDIR_, FDIR_, TESQ_, FESQ_}; // array com as bobinas
-  int sensores[4] = {LD_, CD_, CE_, LE_}; // array com os sensores
-  char debugsensores[4][3] = {"LD","CD","CE","LE"}; //array com os nomes dos sensores para debug
-  int readings[4];  // array para armazenar as leituras dos sensores
-}typedef cIO;
+// Struct para controle das I/Os do robô
+struct controlIO
+{
+  enum Bobinas
+  {
+    TDIR_,
+    FDIR_,
+    TESQ_,
+    FESQ_
+  }; // enumerando para facilitar a identificação das bobinas
+  enum Sensores
+  {
+    LD_,
+    CD_,
+    CE_,
+    LE_
+  }; // enumerando para facilitar a identificação dos sensores
+  int bobinas[4] = {TDIR_, FDIR_, TESQ_, FESQ_};       // array com as bobinas
+  int sensores[4] = {LD_, CD_, CE_, LE_};              // array com os sensores
+  char debugsensores[4][3] = {"LD", "CD", "CE", "LE"}; // array com os nomes dos sensores para debug
+  int readings[4];                                     // array para armazenar as leituras dos sensores
+} typedef cIO;
 
-//CONSTANTES GLOBAIS
-const int DURACAO = 1000; //duração de um ciclo completo em ms
-const int SENSIBILIDADE = 650; //sensibilidade do controle de linha
+// CONSTANTES GLOBAIS-------------------------------------------------------------------------------------------------------------------------------
+const int DURACAO = 1000;      // duração de um ciclo completo em ms
+const int SENSIBILIDADE = 650; // sensibilidade do controle de linha
 
-//VARIÁVEIS GLOBAIS
-int acellCrescente = 0;
-int acellDecrescente = 0;
-int tempo = 0;
-int tempoatual = millis()/1000;
-int tempo_motor = 0;
+// VARIÁVEIS GLOBAIS-------------------------------------------------------------------------------------------------------------------------------
+float acellCrescente = 0;
+float acellDecrescente = 0;
+unsigned long tempo = 0;
+unsigned long tempoatual = millis();
+unsigned long tempo_motor = 0;
 int pwm_increment = 0;
-int acell;
 bool ligado;
-const unsigned long debounce = 50; //debounce do botão em ms
+const unsigned long debounce = 50; // debounce do botão em ms
 
-//FUNÇÕES AUXILIARES
-void direcao(int direitaFrente, int direitaTras, int esquerdaFrente, int esquerdaTras); //função para controlar a direção do robô
-
+// FUNÇÕES AUXILIARES-------------------------------------------------------------------------------------------------------------------------------
+void controleDirecao(int direitaFrente, int direitaTras, int esquerdaFrente, int esquerdaTras); // função para controlar a direção do robô
+void frente();
+void tras();
+void parar();
+void esquerda();
+void direita();
 void leituras();
+int calibracaoMotores(int pwm, int porcentagemTrim); // função para calibrar os motores com base em uma porcentagem de trim
+int trim(int pwm, int porcentagemTrim);              // função para aplicar o trim ao valor do PWM
 
-//INSTÂNCIAS
-cIO robo; //instancia do struct de controle de I/O
-AcksenButton button(BUT,ACKSEN_BUTTON_MODE_NORMAL,debounce,INPUT);  //instancia do botão
+// INSTÂNCIAS-------------------------------------------------------------------------------------------------------------------------------
 
-void setup() {
+cIO robo;                                                             // instancia do struct de controle de I/O
+AcksenButton button(BUT, ACKSEN_BUTTON_MODE_NORMAL, debounce, INPUT); // instancia do botão
+
+//--------------------------------------------------------------------SETUP--------------------------------------------------------//-------------------------------
+void setup()
+{
   Serial.begin(9600);
   // Inicializa as bobinas como saída e os sensores como entrada
-  for (size_t i = 0; i < 4; i++)  //pode se criar uma função para isso algo como robo.init();
+  for (size_t i = 0; i < 4; i++) // pode se criar uma função para isso algo como robo.init();
   {
-    pinMode(robo.bobinas[i],OUTPUT);
-    pinMode(robo.sensores[i],INPUT);
-  //Botão
-  pinMode(BUT,INPUT);  //Botão como entrada
-  //LED
-  pinMode(LED,OUTPUT);  //LED como saída
-  //Inicializa variáveis
-  digitalWrite(LED,LOW);
+    pinMode(robo.bobinas[i], OUTPUT);
+    pinMode(robo.sensores[i], INPUT);
+    // Botão
+    pinMode(BUT, INPUT); // Botão como entrada
+    // LED
+    pinMode(LED, OUTPUT); // LED como saída
+    // Inicializa variáveis
+    digitalWrite(LED, LOW);
   }
 }
-void loop() {
+//--------------------------------------------------------------------LOOP--------------------------------------------------------//-------------------------------
+void loop()
+{
   // put your main code here, to run repeatedly:
-  leituras(); //função para ler os sensores
-  button.refreshStatus(); //atualiza o estado do botão
-  tempoatual = millis()/1000;
+  leituras();             // função para ler os sensores
+  button.refreshStatus(); // atualiza o estado do botão
+  tempoatual = millis() / 1000;
 
-  if (tempoatual - tempo > 1) { // a cada 1 segundo 
-    for (size_t i = 0; i < 4; i++) //loop para imprimir os valores dos sensores
+  if (tempoatual - tempo > 1)
+  {                                // a cada 1 segundo
+    for (size_t i = 0; i < 4; i++) // loop para imprimir os valores dos sensores
     {
       tempo = tempoatual;
-      Serial.print(robo.debugsensores[i]);  //imprime o nome do sensor
-      Serial.print(": "); //imprime dois pontos
-      Serial.println(robo.readings[i]); //imprime o valor do sensor
+      Serial.print(robo.debugsensores[i]); // imprime o nome do sensor
+      Serial.print(": ");                  // imprime dois pontos
+      Serial.println(robo.readings[i]);    // imprime o valor do sensor
     }
   }
 
-  //Cálculo das acelerações oscilatórias
-  acellCrescente = ((0.5)*sin(tempo_motor)+0.5)*255;   //aceleração 1 é o inverso da aceleração 2
-  acellDecrescente = ((0.5)*cos((M_PI/2)+(tempo_motor))+0.5)*255;   //aceleração 2 é o inverso da aceleração 1
+  // Cálculo das acelerações oscilatórias
+  acellCrescente = ((0.5) * sin(tempo_motor) + 0.5);                  // aceleração 1 é o inverso da aceleração 2
+  acellDecrescente = ((0.5) * cos((M_PI / 2) + (tempo_motor)) + 0.5); // aceleração 2 é o inverso da aceleração 1
 
-  //Cálculo das acelerações lineares
-  //int acell1 = ((tempoatual - tempo)/5)*255;
-  //int acell2 = ((tempoatual - tempo)/5)*255;
-  //limita a aceleração máxima
-  if (acellCrescente > 255) {
+  // Cálculo das acelerações lineares
+  // int acell1 = ((tempoatual - tempo)/5)*255;
+  // int acell2 = ((tempoatual - tempo)/5)*255;
+
+  // limita a aceleração máxima
+  if (acellCrescente > 255)
+  {
     acellCrescente = 255;
   }
-  if (acellDecrescente > 255) {
+  if (acellDecrescente > 255)
+  {
     acellDecrescente = 255;
   }
+  if (button.onPressed())
+  {
+    button.refreshStatus();
 
-  if(button.onPressed()) {  //se o botão for pressionado
-      direcao(0, 100, 0, 100); //anda para frente
-      digitalWrite(LED,HIGH); //acende o LED
+    if (button.onPressed())
+    {                                  // se o botão for pressionado
+      controleDirecao(0, 100, 0, 100); // anda para frente
+      digitalWrite(LED, HIGH);         // acende o LED
       ligado = true;
       tempo = millis();
-  }
-  if (ligado) {
-    //se o sensor central direito detectar linha, gira para a esquerda
-    robo.readings[cIO::CD_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(acellCrescente,0,acellDecrescente,0)) : direcao(acellCrescente,0,acellCrescente,0);
-    robo.readings[cIO::CE_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(acellDecrescente,0,acellCrescente,0)): direcao(acellCrescente,0,acellCrescente,0);
-    robo.readings[cIO::LD_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(acellCrescente,0,-acellDecrescente,0)): direcao(acellCrescente,0,acellCrescente,0);
-    robo.readings[cIO::LE_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(-acellDecrescente,0,acellCrescente,0)): direcao(acellCrescente,0,acellCrescente,0);
+    }
+    if (ligado)
+    {
+      // se o sensor central direito detectar linha, gira para a esquerda
+      if (robo.readings[cIO::CD_] < SENSIBILIDADE)
+      {
+        esquerda();
+      }
+      else if (robo.readings[cIO::CE_] < SENSIBILIDADE)
+      {
+        direita();
+      }
+      else if (robo.readings[cIO::LE_] < SENSIBILIDADE || robo.readings[cIO::LD_] < SENSIBILIDADE)
+      {
+      }
+
+      else
+      {
+        frente();
+      }
+    }
   }
 }
+// FUNÇÕES AUXILIARES
+int trim(int pwm, int porcentagemTrim)
+{
+  // Calcula o valor do PWM com base na porcentagem de trim
+  long pwmCalibrado = (long)pwm * porcentagemTrim / 100;
+  return constrain(pwmCalibrado, 0, 255);
+}
 
-void direcao(int direitaFrente, int direitaTras, int esquerdaFrente, int esquerdaTras){
+void calibracaoMotores(int pwm, int porcentagemTrim[])
+{
+  // Calibra os motores com base na porcentagem de trim
+  int pwmCalibrado[4];
+  for (size_t i = 0; i < 4; i++)
+  {
+    pwmCalibrado[i] = trim(pwm, porcentagemTrim[i]);
+  }
+  controleDirecao(pwmCalibrado[0], pwmCalibrado[1], pwmCalibrado[2], pwmCalibrado[3]);
+}
+void controleDirecao(int direitaFrente, int direitaTras, int esquerdaFrente, int esquerdaTras)
+{
   analogWrite(FDIR, direitaFrente);
   analogWrite(TDIR, direitaTras);
   analogWrite(FESQ, esquerdaFrente);
   analogWrite(TESQ, esquerdaTras);
 }
-void leituras(){
+// Funções de movimento - basicas, como otimizar?
+// note que os valores de PWM vão de 0 a 255 - estamos usando valores menores.
+void frente()
+{
+  controleDirecao(100, 0, 100, 0);
+}
+void tras()
+{
+  controleDirecao(0, 100, 0, 100);
+}
+void parar()
+{
+  controleDirecao(0, 0, 0, 0);
+}
+void esquerda()
+{
+  controleDirecao(0, 100, 100, 0);
+}
+void direita()
+{
+  controleDirecao(100, 0, 0, 100);
+}
+// Função para ler os sensores
+void leituras()
+{
   robo.readings[cIO::LD_] = analogRead(LD);
   robo.readings[cIO::CD_] = analogRead(CD);
   robo.readings[cIO::CE_] = analogRead(CE);
