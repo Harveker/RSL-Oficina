6aab4d6ff39d4ba4577ea6edf0071e3d0937bad4
Harveker <franco.endrigo.r@gmail.com>
2025-11-07 22:01:55 -0300

    Refactor control structure and sensor handling in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,139 +11,114 @@
 
 #include <Arduino.h>
 #include <math.h>
+#include "AcksenButton.h"
 
-enum bobinas {TDIR_,FDIR_,TESQ_,FESQ_};
-enum sensores {LD_,CD_,CE_,LE_};
+//ENUMERAÇÕES E STRUCTS
+
+//Struct para controle das I/Os do robô
+struct controlIO {
+  enum Bobinas {TDIR_,FDIR_,TESQ_,FESQ_};   // enumerando para facilitar a identificação das bobinas
+  enum Sensores {LD_,CD_,CE_,LE_};  // enumerando para facilitar a identificação dos sensores
+  int bobinas[4]  = {TDIR_, FDIR_, TESQ_, FESQ_}; // array com as bobinas
+  int sensores[4] = {LD_, CD_, CE_, LE_}; // array com os sensores
+  char debugsensores[4][3] = {"LD","CD","CE","LE"}; //array com os nomes dos sensores para debug
+  int readings[4];  // array para armazenar as leituras dos sensores
+}typedef cIO;
+
+//CONSTANTES GLOBAIS
+const int DURACAO = 1000; //duração de um ciclo completo em ms
+const int SENSIBILIDADE = 650; //sensibilidade do controle de linha
 
 //VARIÁVEIS GLOBAIS
-int ultEstadoBotao;
-int botao;
+int acellCrescente = 0;
+int acellDecrescente = 0;
 int tempo = 0;
-int tempoatual = millis()/100000;
+int tempoatual = millis()/1000;
+int tempo_motor = 0;
 int pwm_increment = 0;
 int acell;
 bool ligado;
+const unsigned long debounce = 50; //debounce do botão em ms
 
-struct sensoresLeitura {
-  int valorLD;
-  int valorCD;
-  int valorCE;
-  int valorLE;
-} leituras;
-  
+//FUNÇÕES AUXILIARES
+void direcao(int direitaFrente, int direitaTras, int esquerdaFrente, int esquerdaTras); //função para controlar a direção do robô
+
+void leituras();
+
+//INSTÂNCIAS
+cIO robo; //instancia do struct de controle de I/O
+AcksenButton button(BUT,ACKSEN_BUTTON_MODE_NORMAL,debounce,INPUT);  //instancia do botão
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   // Inicializa as bobinas como saída e os sensores como entrada
-  for (size_t i = 0; i < 4; i++)
+  for (size_t i = 0; i < 4; i++)  //pode se criar uma função para isso algo como robo.init();
   {
-    pinMode(bobinas(i),OUTPUT);
-    pinMode(bobinas(i),LOW);
-    pinMode(sensores(i),INPUT);
-  }
-  pinMode(BUT,INPUT);
+    pinMode(robo.bobinas[i],OUTPUT);
+    pinMode(robo.sensores[i],INPUT);
+  //Botão
+  pinMode(BUT,INPUT);  //Botão como entrada
   //LED
-  pinMode(LED,OUTPUT);
-  tempo = millis()/100000;
+  pinMode(LED,OUTPUT);  //LED como saída
+  //Inicializa variáveis
+  digitalWrite(LED,LOW);
+  }
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  // Lê os valores dos sensores
-  Serial.println(tempoatual);
-  Serial.print(tempo);
-     valorLE = analogRead(LE);
-     valorCE = analogRead(CE);
-     valorCD = analogRead(CD);
-     valorLD = analogRead(LD);
-  if (tempoatual - tempo > ) {
-     Serial.print("LE: ");
-     Serial.print(valorLE);
-     Serial.print(" | CE: ");
-     Serial.print(valorCE);
-     Serial.print(" | CD: ");
-     Serial.print(valorCD);
-     Serial.print(" | LD: ");
-     Serial.println(valorLD);
-     tempo = millis();
+  leituras(); //função para ler os sensores
+  button.refreshStatus(); //atualiza o estado do botão
+  tempoatual = millis()/1000;
+
+  if (tempoatual - tempo > 1) { // a cada 1 segundo 
+    for (size_t i = 0; i < 4; i++) //loop para imprimir os valores dos sensores
+    {
+      tempo = tempoatual;
+      Serial.print(robo.debugsensores[i]);  //imprime o nome do sensor
+      Serial.print(": "); //imprime dois pontos
+      Serial.println(robo.readings[i]); //imprime o valor do sensor
+    }
   }
-  // Imprime os valores no monitor serial
-     
-  /*
-  if(button_release()){  
-    analogWrite(MDB1,LOW);
-    analogWrite(MDB2,HIGH);
-    analogWrite(MEB1,LOW);
-    analogWrite(MEB2,HIGH);
-  }*/
-  /*
-  if (millis()-tempo == 1000){
-    digitalWrite(MDB1,LOW);
-    digitalWrite(MDB2,LOW);
-    digitalWrite(MEB1,LOW);
-    digitalWrite(MEB2,LOW);
-  }*/
-  tempo = millis();
-  int acell = sin(2*M_PI*tempo)*255/1000;
-  if(button_release()){
-      analogWrite(MDB1,0);
-      analogWrite(MDB2,acell);
-      analogWrite(MEB1,0);
-      analogWrite(MEB2,acell);
+
+  //Cálculo das acelerações oscilatórias
+  acellCrescente = ((0.5)*sin(tempo_motor)+0.5)*255;   //aceleração 1 é o inverso da aceleração 2
+  acellDecrescente = ((0.5)*cos((M_PI/2)+(tempo_motor))+0.5)*255;   //aceleração 2 é o inverso da aceleração 1
+
+  //Cálculo das acelerações lineares
+  //int acell1 = ((tempoatual - tempo)/5)*255;
+  //int acell2 = ((tempoatual - tempo)/5)*255;
+  //limita a aceleração máxima
+  if (acellCrescente > 255) {
+    acellCrescente = 255;
+  }
+  if (acellDecrescente > 255) {
+    acellDecrescente = 255;
+  }
+
+  if(button.onPressed()) {  //se o botão for pressionado
+      direcao(0, 100, 0, 100); //anda para frente
+      digitalWrite(LED,HIGH); //acende o LED
       ligado = true;
       tempo = millis();
   }
   if (ligado) {
-  if ((analogRead(LE) <= 200) && (analogRead(CE) <= 200)) {
-      analogWrite(MDB1,0);
-      analogWrite(MDB2,127);
-      analogWrite(MEB1,0);
-      analogWrite(MEB2,0);
-    } else if ((analogRead(LD) <= 200) && (analogRead(CD) <= 200)) {
-      analogWrite(MDB1,0);
-      analogWrite(MDB2,0);
-      analogWrite(MEB1,0);
-      analogWrite(MEB2,127);
-    } else if ((analogRead(LD) <= 200) && (analogRead(CD) <= 200)) {
-      analogWrite(MDB1,0);
-      analogWrite(MDB2,127);
-      analogWrite(MEB1,0);
-      analogWrite(MEB2,0);
-    } else if ((analogRead(CE) <= 200)) {
-      analogWrite(MDB1, 0);
-      analogWrite(MDB2, 50);
-      analogWrite(MEB1, 0);
-      analogWrite(MEB2, 0);
-    } 
-    else if ((analogRead(CD) <= 200)) {
-      analogWrite(MDB1, 0);
-      analogWrite(MDB2, 0);
-      analogWrite(MEB1, 0);
-      analogWrite(MEB2, 50);
-    }
-    else if ((analogRead(LD) <= 200) && (analogRead(CD) <= 200) && (analogRead(CE) <= 200) && (analogRead(LE) <= 200)) {
-      analogWrite(MDB1, 0);
-      analogWrite(MDB2, 0);
-      analogWrite(MEB1, 0);
-      analogWrite(MEB2, 0);
-      ligado = false;
-    } 
-    
-    else if ((analogRead(LD) >= 900) && (analogRead(CD) >= 900) && (analogRead(CE) > 900) && (analogRead(LE) <= 900)) {
-      analogWrite(MDB1, acell);
-      analogWrite(MDB2, 0);
-      analogWrite(MEB1, acell);
-      analogWrite(MEB2, 0);
-      tempo= millis();
-    } 
+    //se o sensor central direito detectar linha, gira para a esquerda
+    robo.readings[cIO::CD_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(acellCrescente,0,acellDecrescente,0)) : direcao(acellCrescente,0,acellCrescente,0);
+    robo.readings[cIO::CE_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(acellDecrescente,0,acellCrescente,0)): direcao(acellCrescente,0,acellCrescente,0);
+    robo.readings[cIO::LD_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(acellCrescente,0,-acellDecrescente,0)): direcao(acellCrescente,0,acellCrescente,0);
+    robo.readings[cIO::LE_] < SENSIBILIDADE ? ((tempo_motor = tempoatual),direcao(-acellDecrescente,0,acellCrescente,0)): direcao(acellCrescente,0,acellCrescente,0);
   }
 }
 
-bool button_release(){
-   int botao = digitalRead(BUT);
-   if(botao == LOW && ultEstadoBotao == HIGH){
-     delay(50);
-     return true;
-   }
-   ultEstadoBotao = digitalRead(BUT);
+void direcao(int direitaFrente, int direitaTras, int esquerdaFrente, int esquerdaTras){
+  analogWrite(FDIR, direitaFrente);
+  analogWrite(TDIR, direitaTras);
+  analogWrite(FESQ, esquerdaFrente);
+  analogWrite(TESQ, esquerdaTras);
+}
+void leituras(){
+  robo.readings[cIO::LD_] = analogRead(LD);
+  robo.readings[cIO::CD_] = analogRead(CD);
+  robo.readings[cIO::CE_] = analogRead(CE);
+  robo.readings[cIO::LE_] = analogRead(LE);
 }
